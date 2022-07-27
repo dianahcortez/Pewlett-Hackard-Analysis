5605572c96ed2d5e5eeda01fa0262be6ebba9730
dianahcortez <cortezdianah@gmail.com>
2022-07-27 15:34:15 -0600

    adding in final csv file and query verbiage

---
+++ queries.sql
@@ -0,0 +1,85 @@
+queries.sql
+
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE employees (
+    emp_no INT NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,	
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE Titles (
+   emp_no INT NOT NULL,
+   title VARCHAR NOT NULL,
+   from_date DATE NOT NULL,
+   to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, from_date)
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (dept_no),
+    UNIQUE (dept_name)
+);
+
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(4) NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+--when i want to view data from tables
+
+SELECT * FROM titles;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM departments;
+
+SELECT * FROM salaries;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM employees;
+
+--if i have to drop tables
+
+DROP TABLE dept_manager CASCADE;
+
+DROP TABLE dept_emp CASCADE;
+
+DROP TABLE titles CASCADE;
+
+DROP TABLE departments CASCADE;
+
+DROP TABLE employees CASCADE;
+
+DROP TABLE salaries CASCADE;
